53c53925e3dfd2075ee87027098fd3c3cc5a31e7
Dave Michelson <daveism@gmail.com>
2019-06-10 07:12:26 -0400

    fixes id for ga

--- src/scripts/ga.js
+++ src/scripts/ga.js
@@ -4,7 +4,7 @@ const store = new Store({});
 
 // adds a custom google events
 export function googleAnalyticsEvent(action = '', category = '', label = '', value = 0) {
-  gtag('event', action, {  // eslint-disable-line
+  gtag('event', store.getStateItem('uuid'), {  // eslint-disable-line
     event_category: category,
     event_label: label,
     value: `${value}`,
